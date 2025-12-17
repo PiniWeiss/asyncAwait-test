3a38d8f7ebba925a2f1fcc46d6c2c1fe2b0a087e
pini weiss <pini6775@gmail.com>
2025-12-17 12:25:39 +0200

    fixed

--- process/processData.js
+++ process/processData.js
@@ -1,7 +1,7 @@
 import { loadFromJson } from "../searches/readingJson.js";
 
 const records = await loadFromJson("./data/records.json");
-
+const people = await loadFromJson("./data/people.json");
 
 
 
@@ -10,7 +10,7 @@ const records = await loadFromJson("./data/records.json");
 export function putLevelDanger() {
 
   const levels = {};
-  for (let i = 0; i > 0; i++) {
+  for (let i=0; i<records.length; i++) {
     let content = records[i].content.split(" ");
     
     let dangerLevel = content.filter(
@@ -22,9 +22,41 @@ export function putLevelDanger() {
     ).length;
     
     
+    
+    if(dangerLevel>0){
     if (levels[records[i].age]) {
       levels[records[i].age].push(dangerLevel);
     }else levels[records[i].age] = [dangerLevel]
   }
+}
   return levels
 }
+
+
+function sum(arr){
+    let sum = 0;
+
+for (const number of arr) {
+  sum += number;
+}
+return sum
+
+}
+
+
+
+export function calculatAvarageDangerAge(levelObj){
+    const avarageLevels = {}
+    Object.keys(levelObj).forEach(key => levelObj[key] = sum(levelObj[key]))
+return levelObj
+}
+
+export function top3Level(levelObj){
+    const result = Object.entries(levelObj)
+    const sorted = result.sort((a,b)=> b[1] - a[1])
+    return sorted.slice(0,3)
+}
+
+
+
+
